bffeebee2f2a004deae5ca923e8ab2f1ebada840
Wataru Kawashima <kawashima.wataru.32x@st.kyoto-u.ac.jp>
2025-02-10 15:19:58 +0900

    submit調整

--- app.py
+++ app.py
@@ -37,3 +37,6 @@ if __name__ == "__main__":
     # Renderの環境変数 PORT を取得
     port = int(os.environ.get("PORT", 10000))
     socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+
+# Gunicorn 用のエントリポイント
+gunicorn_app = app  # ← これを追加！
